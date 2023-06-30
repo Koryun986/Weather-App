1d630aeb2a19e81b70f45d2b43bbb1372a915708
Koryun <79407889+Koryun986@users.noreply.github.com>
2023-06-30 16:10:17 +0400

    change useDate functionality

--- src/hooks/DateHooks.ts
+++ src/hooks/DateHooks.ts
@@ -4,7 +4,14 @@ const Days = [ "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY
 
 export const useDate = () => {
     const currentDate = new Date();
-    const currentTime = `${currentDate.getHours()} : ${currentDate.getMinutes()}`
+
+    const formateNumberToString = (num: number) => num < 10 ? `0${num}` : num.toString();
+
+    const hours = formateNumberToString(currentDate.getHours())
+    const minutes = formateNumberToString(currentDate.getMinutes())
+    const currentTime = `${hours} : ${minutes}`;
+
+
     const weekDay = Days[currentDate.getDay() - 1];
     
     const date = `${weekDay} ${currentTime}`
